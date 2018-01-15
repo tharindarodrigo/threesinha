fa2d0239e06d5da10920b11767902a77767fde56
tharidnarodrigo <tharindarodrigo@gmail.com>
2018-01-15 12:36:50 +0530

    token management

--- database/seeds/CredentialsTableSeeder.php
+++ database/seeds/CredentialsTableSeeder.php
@@ -12,7 +12,7 @@ class CredentialsTableSeeder extends Seeder
     public function run()
     {
         \App\Credential::insert([
-            ['credential' => 'refreshToken', 'value' => 'b4f97f9e-b606-380d-a06b-348d9c9f8e00'],
+            ['credential' => 'refreshToken', 'value' => '91b6b963-823d-32a2-a969-57bb94f6a467'],
             ['credential' => 'accessToken', 'value' => '3f16a0df-8073-3cd8-adef-1ffe740646a9'],
             ['credential' => 'expiry_time', 'value' => 0],
             ['credential' => 'user_name', 'value' => 'dialogiotdeveloper@gmail.com'],
