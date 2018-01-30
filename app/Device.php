a645382334d5020dae234baf512648099f589ebd
tharidnarodrigo <tharindarodrigo@gmail.com>
2018-01-30 18:13:54 +0530

    Remote Control pushed

--- app/Device.php
+++ app/Device.php
@@ -17,7 +17,7 @@ class Device extends Model
         $token->authorize();
 
         $auth = session()->get('AUTH');
-        $authcr = json_encode($auth);
+        $authcr = json_decode($auth);
         $auth= json_decode($authcr);
         dd($auth);
         $curl = curl_init();
